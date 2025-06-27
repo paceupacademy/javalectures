ff253a76a16d0296ef2beb13f91baf376114be23
shraddhasd <scd.shraddha@gmail.com>
2025-06-27 13:55:11 -0400

    Assignment

---
+++ MyFirstJavaProject/src/com/paceup/day10/ArrayExample.java
@@ -0,0 +1,47 @@
+package com.paceup.day10;
+
+import java.util.Arrays;
+import java.util.Scanner;
+
+public class ArrayExample {
+
+	public static void main(String[] args) {
+		// Write a program to initialize an integer array and print all elements.
+		/*
+		 * int[] array = { 1, 2, 3, 4, 5 }; System.out.println("for loop"); for (int i =
+		 * 0; i < array.length; i++) { System.out.println(array[i]); }
+		 * System.out.println("forEach loop"); for (int i : array) {
+		 * System.out.println(i); }
+		 */
+
+		// Create a program that takes 5 numbers from the user and stores them in an
+		// array. Then print their average.
+		/*
+		 * Scanner sc = new Scanner(System.in); int num = 0; int[] arr = new int[5]; for
+		 * (int i = 0; i < 5; i++) { System.out.println("Enter positive number:"); num =
+		 * sc.nextInt(); arr[i] = num; }
+		 * 
+		 * System.out.println("Array:"); for (int i : arr) { System.out.println(i); }
+		 */
+
+		// Write a method to reverse the elements of an array.
+		/*
+		 * int[] arr_1 = { 1, 2, 3, 4, 5 }; int[] arr_reverse = new int[arr_1.length];
+		 * int j = 0; for(int i= arr_1.length-1;i>=0;i--) { arr_reverse[j] = arr_1[i];
+		 * j++; }
+		 */
+
+		// Sort a string array alphabetically using Arrays.sort().
+
+		/*
+		 * String[] str_arr = { "apple", "dog", "map", "cat", "ball" };
+		 * Arrays.sort(str_arr); for (String str : str_arr) { System.out.println(str); }
+		 */
+
+		// Write a program to find the maximum and minimum value in an integer array.
+		
+		
+		
+	}
+
+}
