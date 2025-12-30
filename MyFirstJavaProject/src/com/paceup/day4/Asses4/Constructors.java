39d3fc9fd186f716de6ef662c39ddf2552d71348
Shree <Shree@ThinkBook>
2025-12-30 21:06:27 +0530

    Assessment 4 completed.

---
+++ MyFirstJavaProject/src/com/paceup/day4/Asses4/Constructors.java
@@ -0,0 +1,23 @@
+//Write a class with default and parameterized constructors.
+
+package com.paceup.day4.Asses4;
+
+class Constructors {
+
+	// Default constructors
+	public Constructors() {
+		System.out.println("This is Default Constructors...");
+	}
+
+	// Parameterized constructors
+	Constructors(int m) {
+		System.out.println("This is Parameterized Constructors...");
+		System.out.println("Parameterized Constructors value: " + m);
+	}
+
+	public static void main(String[] args) {
+		Constructors dc = new Constructors();
+		Constructors cc = new Constructors(10);
+	}
+
+}
