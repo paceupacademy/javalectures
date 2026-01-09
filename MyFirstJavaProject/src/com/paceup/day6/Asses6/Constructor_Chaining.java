8d9eec3526d50aac9aecc0ded9ab0191d5fe5094
Shree <Shree@ThinkBook>
2026-01-09 21:52:23 +0530

    Assessment 7 completed

--- MyFirstJavaProject/src/com/paceup/day6/Asses6/Constructor_Chaining.java
+++ MyFirstJavaProject/src/com/paceup/day6/Asses6/Constructor_Chaining.java
@@ -1,6 +1,35 @@
 //4. Show constructor chaining using super() and this().
 package com.paceup.day6.Asses6;
 
+/*class A {
+	A(String name) { // parent class constructors
+		System.out.println("Recieved name: " + name+" from B's parameterized cosntructor"); //3rd
+	}
+}
+
+class B extends A {
+	B() { // child class constructors
+		this("XYZ"); // calls B(STring)  //1st
+		
+		System.out.println("Default constructor finished."); //5th
+	}
+
+	B(String name) {
+		super(name); // calls A(String) //2nd	
+		System.out.println("Received name from Default Constructor..."); //4th
+		
+	} 
+}
+
+public class Constructor_Chaining {
+
+	public static void main(String[] args) {
+		B obj = new B();
+
+	}
+
+}*/
+
 class A {
 	A(String name) { // parent class constructors
 		System.out.println("A name: " + name);
---
+++ MyFirstJavaProject/src/com/paceup/day7/Asses7/PrivateOverridden.java
@@ -0,0 +1,28 @@
+//8. Show that private methods are not overridden by writing a base and derived class example.
+
+package com.paceup.day7.Asses7;
+
+class Base{
+	private void display() {
+		System.out.println("Base class display method...");
+	}
+}
+
+class Derived extends Base{
+	 private void display() {
+		System.out.println("Dervied class show method...");
+	}
+}
+
+public class PrivateOverridden {
+
+	public static void main(String[] args) {
+		
+		Base objBase = new Base();
+		//objBase.display(); //Compile Error
+		
+		Derived objDerived = new Derived();
+		//objDerived.display(); //Compile Error
+	}
+
+}
